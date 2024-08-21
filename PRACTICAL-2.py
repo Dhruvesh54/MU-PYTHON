77415356ffb278638f7b10455a37a8dfaf8be72e
Dhruvesh <97716927+Dhruvesh54@users.noreply.github.com>
2024-08-21 09:50:44 +0530

    Update program 2-3

---
+++ PRACTICAL-2.py
@@ -0,0 +1,8 @@
+# 2	Write a program to demonstrate different number datatypes in python.
+a = 54
+b = 54.2
+c = "DK Patel"
+
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ PRACTICAL-3.py
@@ -0,0 +1,1 @@
+#3 Write a program that uses for loop to print all the odd, even numbers in the range input   by user.
