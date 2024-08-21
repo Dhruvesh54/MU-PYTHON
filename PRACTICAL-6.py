87fca3424b38c727907cfd275188017ec241459d
Dhruvesh <97716927+Dhruvesh54@users.noreply.github.com>
2024-08-21 10:43:55 +0530

    6 program completed

---
+++ PRACTICAL-6.py
@@ -0,0 +1,6 @@
+# 6	Write a program to demonstrate working with tuples in python.
+Tuple1 = ('DK', 'Patel', 9726411336)
+print("Tuple is:" + str(Tuple1))
+
+print("Length is: " + str(len(Tuple1)))
+print("Count is: " + str(Tuple1.count('Patel')))
