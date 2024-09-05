c197c5a81b715050745d3f2db8325bdbe23fc9a3
Dhruvesh <97716927+Dhruvesh54@users.noreply.github.com>
2024-09-05 11:00:38 +0530

    Update

--- PRACTICAL-5.py
+++ PRACTICAL-5.py
@@ -1,7 +1,15 @@
 # 5 Write a python program to create, append and remove lists in python.
-My_List = ["DKpatel",4,"3EP1",9726411336,30]
+My_List = ["Friends",4,"3EP2",1234567890,123]
+print(My_List)
+My_List.append("India")
+print(My_List)
+My_List.remove(1234567890)
+print(My_List)
+'''
+My_List = ["DKpatel",4,"3EP2",9726411336,30]
 print(My_List)
 My_List.append("India")
 print(My_List)
 My_List.remove(9726411336)
 print(My_List)
+'''
--- PRACTICAL-6.py
+++ PRACTICAL-6.py
@@ -1,6 +1,14 @@
 # 6	Write a program to demonstrate working with tuples in python.
+Tuple1 = ('Hello', 'Friends', 1234567890)
+print("Tuple is:" + str(Tuple1))
+
+print("Length is: " + str(len(Tuple1)))
+print("Count is: " + str(Tuple1.count('Friends')))
+
+'''
 Tuple1 = ('DK', 'Patel', 9726411336)
 print("Tuple is:" + str(Tuple1))
 
 print("Length is: " + str(len(Tuple1)))
 print("Count is: " + str(Tuple1.count('Patel')))
+'''
